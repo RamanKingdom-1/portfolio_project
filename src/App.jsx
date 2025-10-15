b274d7d6e3eaae4a739b3affb95aa8ee77c4b5ca
adithya_raman <adithyaraman76@gmail.com>
2025-10-15 03:03:26 -0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { Box } from "@mui/material";
 import { WordlePage } from "./Wordle/WordlePage";
 import { About } from "./About Me/About";
 import { ProjectsPage } from "./Projects-Page/projects";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   const title = [1, 2, 3, 4];
   return (
     <Router>
       <Navbar />
+      <Analytics/>
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/projects" element={<ProjectsPage />} />
